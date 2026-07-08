f38edb358da5db49e4a20c983931f0ed4ec56cf2
Andri 33 <saputroandri1991@gmail.com>
2026-07-08 10:13:15 +0700

    java(variables and operators): declare and initializing variables

---
+++ src/CarInfo.java
@@ -0,0 +1,19 @@
+public class CarInfo {
+    public static void main(String[] args) {
+        String make = "Ford";
+
+        String model = "Mustang";
+
+        int year = 2022;
+
+        double mileage = 15002.5;
+
+        boolean isAutomatic = true;
+
+        System.out.println("Car make: " + make);
+        System.out.println("Car model: " + model);
+        System.out.println("Car year: " + year);
+        System.out.println("Car mileage: " + mileage + " miles");
+        System.out.println("Is the Car automatic: " + isAutomatic);
+    }
+}
