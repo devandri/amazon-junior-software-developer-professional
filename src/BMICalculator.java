f4e2b4f5646e530842f574b908b5c85f9c97b1b9
Andri 33 <saputroandri1991@gmail.com>
2026-07-15 15:30:29 +0700

    java(class-object): calculate BMI

---
+++ src/BMICalculator.java
@@ -0,0 +1,45 @@
+// BMICalculator class is a utility class that's used to calculate the BMI value in both Imperial and Metric systems
+public class BMICalculator {
+
+    // Please declare 4 attributes/properties to represent weight in pounds, height in inches, weight in kilos and height in meters
+    private double weightInPounds;
+    private double heightInInches;
+    private double weightInKilos;
+    private double heightInMeters;
+
+
+    // Please use this default no arguments constructor to initialize the 4 properties to some initial values
+    public BMICalculator() {
+        weightInPounds = 127.8;
+        heightInInches = 70;
+        weightInKilos = 58;
+        heightInMeters = 1.73;
+    }
+
+    // Please implement the calculateBmiImperial() method to take in weight and height to calculate and return the BMI value in the Imperial system
+    public double calculateBmiImperial() {
+        return (703 * weightInPounds) / (heightInInches * heightInInches);
+    }
+
+    // Please implement the calculateBmiMetric() method to take in weight and height to calculate and return the BMI value in the Metric system
+    public double calculateBmiMetric() {
+        return weightInKilos / (heightInMeters * heightInMeters);
+    }
+
+    // Please implement the getBMICategory() method so that it takes the BMI value and returns the BMI category based on it
+    public String getBMICategory(double bmi) {
+        String bmiCategory;
+
+        if (bmi < 18.5) {
+            bmiCategory = "Underweight";
+        } else if (bmi < 25) {
+            bmiCategory = "Normal Weight";
+        } else if (bmi < 30) {
+            bmiCategory = "Overweight";
+        } else {
+            bmiCategory = "Obese";
+        }
+
+        return bmiCategory;
+    }
+}
