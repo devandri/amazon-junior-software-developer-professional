9e24d0ffed602ef0f79bf2d3607ca17dc223b7ae
Andri 33 <saputroandri1991@gmail.com>
2026-07-16 09:37:09 +0700

    java(class-object): simple calculator

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator{
+
+    int calc(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    double calc(double number1, double number2) {
+        return number1 * number2;
+    }
+}
---
+++ src/ModuleQuiz_M3L3.java
@@ -0,0 +1,9 @@
+public class ModuleQuiz_M3L3 {
+    public static void main(String []args) {
+
+        Calculator calcObject = new Calculator();
+
+        System.out.println( calcObject.calc(2, 1) );
+
+    }
+}
